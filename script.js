a1d28601ecfa1bae1e63feb8ae481a24ef8c743a
Dan Karsenty <danchlomo@gmail.com>
2019-05-30 16:46:28 +0300

    fourth commit

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-// Créez un évènement qui permet de cliquer sur le bouton ajout_animal.
-// Dans cette fonction, appelez la fonction addAnimal, dans laquelle les paramètres passés
-// doivent être les valeurs des inputs « name_input » et « country_input ».
+// Créez une fonction renderAnimaux qui ne prend aucun paramètre. Cette fonction doit
+// ajouter chaque animal de la liste, à la balise <ul> du fichier HTML
 
-
-
+animaux = [];
 
 function addAnimal (name,country) {
-
+	animal = {
+		'animalName': name,
+		'animalCountry':country
+	}
+	animaux.push(animal)
+	console.log(animaux)
 }
 
 $(".ajout_animal").on("click", function(){
-  
+  var name = $('.name_input').val();
+  var country = $('.country_input').val();
+  addAnimal(name,country)
 });
+
+
+function renderAnimaux () {
+	
+}
+
+
+
