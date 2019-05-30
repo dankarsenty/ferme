f0c02601e59ef737b520b5ae0ec6b4798ab8b900
Dan Karsenty <danchlomo@gmail.com>
2019-05-30 15:41:48 +0300

    third commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Créez un évènement qui permet de cliquer sur le bouton ajout_animal.
+// Dans cette fonction, appelez la fonction addAnimal, dans laquelle les paramètres passés
+// doivent être les valeurs des inputs « name_input » et « country_input ».
+
+
+
+
+function addAnimal (name,country) {
+
+}
+
+$(".ajout_animal").on("click", function(){
+  
+});
